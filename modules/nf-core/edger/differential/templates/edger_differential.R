dd4348bfe593d30dbd0c01416a6fc392d1d5f7f4
Suzanne Jin <suzanne.jin.yao@gmail.com>
2025-01-10 17:38:47 +0000

    add check contrast code

--- modules/nf-core/edger/differential/templates/edger_differential.R
+++ modules/nf-core/edger/differential/templates/edger_differential.R
@@ -78,39 +78,47 @@ read_delim_flexible <- function(file, header = TRUE, row.names = 1, check.names 
 # Set defaults and classes
 
 opt <- list(
-    output_prefix             = ifelse('$task.ext.prefix' == 'null', '$meta.id', '$task.ext.prefix'),
+    output_prefix              = ifelse('$task.ext.prefix' == 'null', '$meta.id', '$task.ext.prefix'),
 
     # input files
-    count_file                = '$counts',
-    sample_file               = '$samplesheet',
+    count_file                 = '$counts',
+    sample_file                = '$samplesheet',
 
     # features and sample columns
-    features_id_col           = 'gene_id',                  # column name of feature ids
-    obs_id_col                = 'sample',                   # column name of observation ids
+    features_id_col            = 'gene_id',                  # column name of feature ids
+    obs_id_col                 = 'sample',                   # column name of observation ids
 
     # comparison groups
-    contrast_variable         = "$contrast_variable",       # column name of contrast variable
-    reference_group           = "$reference",               # reference group for contrast variable
-    target_group              = "$target",                  # target group for contrast variable
-    blocking_variables        = NULL                        # column name of blocking variables
+    contrast_variable          = "$contrast_variable",       # column name of contrast variable
+    reference_group            = "$reference",               # reference group for contrast variable
+    target_group               = "$target",                  # target group for contrast variable
+    blocking_variables         = NULL                        # column name of blocking variables
+
+    # exclude samples
+    subset_to_contrast_samples = FALSE,                      # keep only the samples involved in the contrast
+    exclude_samples_col        = NULL,                       # exclude samples
+    exclude_samples_values     = NULL,                       # exclude samples with given values
 
     # other parameters
-    round_digits              = NA,                         # number of digits to round results
-    ncores                    = as.integer('$task.cpus')    # number of cpus
+    round_digits               = NA,                         # number of digits to round results
+    ncores                     = as.integer('$task.cpus')    # number of cpus
 )
 
 opt_types <- list(
-    output_prefix      = 'character',
-    count_file         = 'character',
-    sample_file        = 'character',
-    features_id_col    = 'character',
-    obs_id_col         = 'character',
-    contrast_variable  = 'character',
-    reference_group    = 'character',
-    target_group       = 'character',
-    blocking_variables = 'character',
-    round_digits       = 'numeric',
-    ncores             = 'numeric'
+    output_prefix              = 'character',
+    count_file                 = 'character',
+    sample_file                = 'character',
+    features_id_col            = 'character',
+    obs_id_col                 = 'character',
+    contrast_variable          = 'character',
+    reference_group            = 'character',
+    target_group               = 'character',
+    blocking_variables         = 'character',
+    subset_to_contrast_samples = 'character',
+    exclude_samples_col        = 'character',
+    exclude_samples_values     = 'character',
+    round_digits               = 'numeric',
+    ncores                     = 'numeric'
 )
 
 # Apply parameter overrides
@@ -210,7 +218,112 @@ if (length(missing_samples) > 0) {
 
 ################################################
 ################################################
-## Run edgeR processes                       ##
+## CHECK CONTRAST SPECIFICATION               ##
+################################################
+################################################
+
+contrast_variable <- make.names(opt\$contrast_variable)
+blocking.vars <- c()
+
+if (!contrast_variable %in% colnames(sample.sheet)) {
+    stop(
+        paste0(
+        'Chosen contrast variable \"',
+        contrast_variable,
+        '\" not in sample sheet'
+        )
+    )
+} else if (any(!c(opt\$reference_level, opt\$target_level) %in% sample.sheet[[contrast_variable]])) {
+    stop(
+        paste(
+        'Please choose reference and target levels that are present in the',
+        contrast_variable,
+        'column of the sample sheet'
+        )
+    )
+} else if (!is.null(opt\$blocking_variables)) {
+    blocking.vars = make.names(unlist(strsplit(opt\$blocking_variables, split = ';')))
+    if (!all(blocking.vars %in% colnames(sample.sheet))) {
+        missing_block <- paste(blocking.vars[! blocking.vars %in% colnames(sample.sheet)], collapse = ',')
+        stop(
+            paste(
+                'Blocking variables', missing_block,
+                'do not correspond to sample sheet columns.'
+            )
+        )
+    }
+}
+
+# Handle conflicts between blocking variables and block
+
+if (!is.null(opt\$block) && !is.null(opt\$blocking_variables)) {
+    if (opt\$block %in% blocking.vars) {
+        warning(paste("Variable", opt\$block, "is specified both as a fixed effect and a random effect. It will be treated as a random effect only."))
+        blocking.vars <- setdiff(blocking.vars, opt\$block)
+        if (length(blocking.vars) == 0) {
+            opt\$blocking_variables <- NULL
+        } else {
+            opt\$blocking_variables <- paste(blocking.vars, collapse = ';')
+        }
+    }
+}
+
+# Optionally, subset to only the samples involved in the contrast
+
+if (opt\$subset_to_contrast_samples) {
+    sample_selector <- sample.sheet[[contrast_variable]] %in% c(opt\$target_level, opt\$reference_level)
+    selected_samples <- sample.sheet[sample_selector, opt\$sample_id_col]
+    count.table <- count.table[, selected_samples]
+    sample.sheet <- sample.sheet[selected_samples, ]
+}
+
+# Optionally, remove samples with specified values in a given field (probably
+# don't use this as well as the above)
+
+if ((! is.null(opt\$exclude_samples_col)) && (! is.null(opt\$exclude_samples_values))) {
+    exclude_values = unlist(strsplit(opt\$exclude_samples_values, split = ';'))
+
+    if (! opt\$exclude_samples_col %in% colnames(sample.sheet)){
+        stop(paste(opt\$exclude_samples_col, ' specified to subset samples is not a valid sample sheet column'))
+    }
+
+    print(paste0('Excluding samples with values of ', opt\$exclude_samples_values, ' in ', opt\$exclude_samples_col))
+    sample_selector <- ! sample.sheet[[opt\$exclude_samples_col]] %in% exclude_values
+
+    selected_samples <- sample.sheet[sample_selector, opt\$sample_id_col]
+    count.table <- count.table[, selected_samples]
+    sample.sheet <- sample.sheet[selected_samples, ]
+}
+
+################################################
+################################################
+## Build the Model Formula                    ##
+################################################
+################################################
+
+# Build the model formula with blocking variables first
+model_vars <- c()
+
+if (!is.null(opt\$blocking_variables)) {
+    # Include blocking variables (including pairing variables if any)
+    model_vars <- c(model_vars, blocking.vars)
+}
+
+# Add the contrast variable at the end
+model_vars <- c(model_vars, contrast_variable)
+
+# Construct the model formula
+model <- paste('~ 0 +', paste(model_vars, collapse = '+'))
+
+# Make sure all the appropriate variables are factors
+vars_to_factor <- model_vars  # All variables in the model need to be factors
+for (v in vars_to_factor) {
+    sample.sheet[[v]] <- as.factor(sample.sheet[[v]])
+}
+
+################################################
+################################################
+## Run edgeR processes                        ##
 ################################################
 ################################################
 
